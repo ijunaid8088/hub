12a954dd944724907cc0949d8dd489fc7375cbde
Junaid Farooq <junaid@evercam.io>
2016-11-10 23:29:08 +0500

    Added username to User view

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -2,16 +2,19 @@ defmodule Hub.UserView do
   use Hub.Web, :view
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, Hub.UserView, "user.json")}
+    %{user: render_one(user, Hub.UserView, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       firstname: user.firstname,
       lastname: user.lastname,
       email: user.email,
       company_name: user.company_name,
       company_url: user.company_url,
-      company_role: user.company_role}
+      company_role: user.company_role,
+      username: user.username
+    }
   end
 end
