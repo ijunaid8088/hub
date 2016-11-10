735c331465010ef1b0cb032d8dc4f85beec97393
Junaid Farooq <junaid@evercam.io>
2016-11-10 23:10:59 +0500

    Added validations in User model

--- web/models/user.ex
+++ web/models/user.ex
@@ -14,6 +14,19 @@ defmodule Hub.User do
     timestamps()
   end
 
+  defp encrypt_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password, hash_password(password))
+      _ ->
+        changeset
+    end
+  end
+
+  def hash_password(password) do
+    Comeonin.Bcrypt.hashpass(password, Comeonin.Bcrypt.gen_salt(12, true))
+  end
+
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
@@ -21,5 +34,13 @@ defmodule Hub.User do
     struct
     |> cast(params, [:firstname, :lastname, :email, :company_name, :company_url, :company_role, :token, :password])
     |> validate_required([:firstname, :lastname, :email, :company_name, :company_url, :company_role, :token, :password])
+    |> validate_length(:password, [min: 6, message: "Password should be at least 6 character(s)."])
+    |> encrypt_password
+    |> update_change(:username, &String.downcase/1)
+    |> update_change(:email, &String.downcase/1)
+    |> update_change(:firstname, &String.trim/1)
+    |> update_change(:lastname, &String.trim/1)
+    |> validate_length(:firstname, [min: 2, message: "Firstname should be at least 2 character(s)."])
+    |> validate_length(:lastname, [min: 2, message: "Lastname should be at least 2 character(s)."])
   end
 end
