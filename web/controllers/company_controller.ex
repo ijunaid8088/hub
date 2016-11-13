1247852a3ccf03df210ed79499cf1e4d9b15fb72
Junaid Farooq <junaid@evercam.io>
2016-11-13 18:10:36 +0500

    Added Index method for all companies

--- web/controllers/company_controller.ex
+++ web/controllers/company_controller.ex
@@ -3,6 +3,11 @@ defmodule Hub.CompanyController do
 
   alias Hub.Company
 
+  def index(conn, _params) do
+    companies = Repo.all(Company)
+    render(conn, "index.json", companies: companies)
+  end
+
   def create(conn, company_params) do
     changeset = Company.changeset(%Company{}, company_params)
 
