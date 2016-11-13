3e4091201d6e1b72aeb9d94ca27c06dab0c1255f
Junaid Farooq <junaid@evercam.io>
2016-11-13 18:11:43 +0500

    Added index view for User

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,8 +1,12 @@
 defmodule Hub.UserView do
   use Hub.Web, :view
 
+  def render("index.json", %{users: users}) do
+    %{users: render_many(users, __MODULE__, "user.json")}
+  end
+
   def render("show.json", %{user: user}) do
-    %{user: render_one(user, Hub.UserView, "user.json")}
+    %{user: render_one(user, __MODULE__, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
