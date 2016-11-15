68ea808377c54b320de971c2e9eb1a98b9e29584
Junaid Farooq <junaid@evercam.io>
2016-11-15 19:09:23 +0500

    Added password validator.

--- web/validators/user_validators.ex
+++ web/validators/user_validators.ex
@@ -1,12 +1,24 @@
 defmodule Hub.UserValidators do
-	alias Hub.User
+  alias Hub.User
 
-	def validate_token(token) do
-		with %Hub.User{} <- User.by_token(token) do
-			:ok
-		else
-			nil ->
-				{:invalid}
-		end
-	end
+  def validate_token(token) do
+    with %Hub.User{} <- User.by_token(token) do
+      :ok
+    else
+      nil ->
+        {:invalid}
+    end
+  end
+
+  def validate_password(username, password) do
+    with %Hub.User{password: u_password} <- User.by_username(username) do
+      case Comeonin.Bcrypt.checkpw(password, u_password) do
+        true  -> :ok
+        false -> {:invalid}
+       end
+    else
+      nil ->
+        {:invalid}
+    end
+  end
 end
