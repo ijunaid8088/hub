6d4f9a85bfadf2e0d6be900c8e5e231e2c3fed02
Junaid Farooq <junaid@evercam.io>
2017-01-18 00:52:12 +0500

    Added first log boolean

--- priv/repo/migrations/20170117194344_add_fields_to_users.exs
+++ priv/repo/migrations/20170117194344_add_fields_to_users.exs
@@ -2,6 +2,8 @@ defmodule Hub.Repo.Migrations.AddFieldsToUsers do
   use Ecto.Migration
 
   def change do
-
+    alter table(:users) do
+      add :first_login, :boolean, default: true
+    end
   end
 end
